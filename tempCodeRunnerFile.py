01a986a72ad56f995d4fef6759d53be25163adcd
krizo69 <kristian.liubomirov09@gmail.com>
2025-06-03 23:26:29 +0300

    Добавени полове, enter, user list
    Добавен е задължителен избор на пол и съотвен цвят в чата за него.  Списък на участници в дадената стая и възможността съобщенията да се пращат с Enter бутон а не само чрез send.

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -7,12 +7,11 @@ socketio = SocketIO(app)
 
 
 rooms = {
-    "8": {"members": 0, "messages": [], "users": {}},  # dobavqm users za spisuk s useri
-    "9": {"members": 0, "messages": [], "users": {}},  # dobavqm users za spisuk s useri
-    "10": {"members": 0, "messages": [], "users": {}},  # dobavqm users za spisuk s useri   
-    "10": {"members": 0, "messages": [], "users": {}},  # dobavqm users za spisuk s useri
-    "11": {"members": 0, "messages": [], "users": {}},  # dobavqm users za spisuk s useri
-    "12": {"members": 0, "messages": [], "users": {}},  # dobavqm users za spisuk s useri
+    "8": {"members": 0, "messages": []},
+    "9": {"members": 0, "messages": []},
+    "10": {"members": 0, "messages": []},
+    "11": {"members": 0, "messages": []},
+    "12": {"members": 0, "messages": []},
 }
 
 @app.route("/", methods=["GET"])
@@ -25,7 +24,6 @@ def join():
     if request.method == "POST":
         name = request.form.get("name")
         code = request.form.get("code")
-        gender = request.form.get("gender") #promqnata mi za pola
 
         if not name:
             return render_template("home.html", error="Please enter a name.", code=code, name=name)
@@ -35,7 +33,6 @@ def join():
 
         session["room"] = code
         session["name"] = name
-        session["gender"] = gender #promqnata mi za pola
         return redirect(url_for("room"))
     ## molq ako stane problem opravi route functionality 
     return render_template("home.html")
@@ -61,8 +58,7 @@ def message(data):
     ##ne pipai
     content = {
         "name": session.get("name"),
-        "message": data["data"],
-        "gender": session.get("gender") #promqnata mi za pola
+        "message": data["data"]
     }
     send(content, to=room)
     rooms[room]["messages"].append(content)
@@ -72,7 +68,6 @@ def message(data):
 def connect():
     room = session.get("room")
     name = session.get("name")
-    gender= session.get("gender") #promqnata mi za pola
     if not room or not name:
         return
     if room not in rooms:
@@ -80,29 +75,20 @@ def connect():
         return
     
     join_room(room)
-    rooms[room]["users"][name] = {"gender": gender} #sushto promqna
-    send({"name": name, "message": "has entered the room", "gender":gender}, to=room)##dobavih "gender:gender"
+    send({"name": name, "message": "has entered the room"}, to=room)
     rooms[room]["members"] += 1
-    user_list = [{"name": name, "gender": info["gender"]} for name, info in rooms[room]["users"].items()]
-    socketio.emit("user_list", user_list, to=room)#izpisvane na polovete v spisuka
-
     print(f"{name} joined room {room}")
     ##ako shte pipash neshto me pitai tvurde mn vreme si izubih s tova
 @socketio.on("disconnect")
 def disconnect():
     room = session.get("room")
     name = session.get("name")
-    gender = session.get("gender") #promqna
     leave_room(room)
 
     if room in rooms:
         rooms[room]["members"] -= 1
-        rooms[room]["users"].pop(name, None)  
-        user_list = [{"name": name, "gender": info["gender"]} for name, info in rooms[room]["users"].items()]
-        socketio.emit("user_list", user_list, to=room)#izpisvane na polovete v spisuka
 
- 
-    send({"name": name, "message": "has left the room", "gender":gender}, to=room)##lubimata mi chast ##adnah pak
+    send({"name": name, "message": "has left the room"}, to=room)##lubimata mi chast
     print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":
